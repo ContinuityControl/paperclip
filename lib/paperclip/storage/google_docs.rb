b60b3ceef86b5121e7fd46bb8b6cae48b8d1ed5d
yonkeltron <yonkeltron@gmail.com>
2010-10-26 09:57:46 -0400

    added google docs storage
    Signed-off-by: yonkeltron <yonkeltron@gmail.com>

---
+++ lib/paperclip/storage/google_docs.rb
@@ -0,0 +1,68 @@
+module Paperclip
+  module Storage
+    module Google
+   
+      def self.extended base
+        begin
+          require 'gdata'
+        rescue LoadError => e
+          e.message << " (You may need to install the gdata gem)"
+          raise e
+        end
+
+        base.instance_eval do
+          @google_credentials = @options[:cred_proc]
+          @google_credentials = @google_credentials.call(self) if @google_credentials.is_a?(Proc)
+          @google_url = 'https://docs.google.com/feeds/documents/private/fulll'
+
+          @client = GData::Client::DocList.new
+          @client.authsub_token = @google_credentials
+        end        
+      end
+
+
+      def exists?(style = default_style)
+        original_filename
+      end
+
+      def to_file style = default_style
+        #return @queued_for_write[style] if @queued_for_write[style]
+        filename = original_filename
+        extname  = File.extname(filename)
+        basename = File.basename(filename, extname)
+        file = Tempfile.new(basename, extname)
+        file.write(path(style))
+        file.rewind
+        return file
+      end
+
+      def flush_writes
+        @queued_for_write.each do |style, file|
+          begin
+            if !@saved
+              log("saving #{path(style)}")
+              
+              result = @client.post_file(@google_url, file.path, instance_read(:content_type))
+              doc = Nokogiri::XML(result.body)
+              path = doc.css('link[rel=alternate]').first['href']
+
+              @saved = true
+            end
+          rescue Exception => e
+            # something
+          end
+        end
+      end
+
+      def flush_deletes
+        @queued_for_delete.each do |path|
+          begin
+            @client.delete(path)
+          rescue Exception => e
+            # something else
+          end
+        end
+      end
+    end
+  end
+end
